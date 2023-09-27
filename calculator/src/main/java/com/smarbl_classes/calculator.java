d819a6ac6842cead819eec5121b27deaaa3d2800
Swati Wanjari <146084843+swati-wanjari@users.noreply.github.com>
2023-09-27 15:35:14 +0530

    Swati (#2)
    * my first contribution to calci
    
    * my first commit
    
    * project_calculator
    
    * Base Code
    
    * deleted
    
    ---------
    
    Co-authored-by: DeekshaSahu1406 <deeksha.sahu@smarbl.com>

--- calculator/src/main/java/com/smarbl_classes/calculator.java
+++ calculator/src/main/java/com/smarbl_classes/calculator.java
@@ -1,6 +1,25 @@
 package com.smarbl_classes;
 
-public class calculator {
+import java.util.Scanner;
 
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("Available operations:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+        System.out.print("Select an operation (1/2/3/4): ");
+
+        int choice = scanner.nextInt();
+
+        System.out.print("Enter the first number: ");
+
+
+        scanner.close();
+    }
 
 }
