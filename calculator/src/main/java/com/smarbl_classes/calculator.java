060bc90cad8e2b218d80faae52f20017b3316d04
Swati Wanjari <146084843+swati-wanjari@users.noreply.github.com>
2023-09-27 16:23:06 +0530

    Substract (#5)
    * my first contribution to calci
    
    * my first commit
    
    * project_calculator
    
    * Base Code
    
    * deleted
    
    * untracked
    
    * deleted
    
    * Added Addition Function
    
    * sub
    
    ---------
    
    Co-authored-by: DeekshaSahu1406 <deeksha.sahu@smarbl.com>

--- calculator/src/main/java/com/smarbl_classes/calculator.java
+++ calculator/src/main/java/com/smarbl_classes/calculator.java
@@ -12,6 +12,7 @@ public class calculator {
         System.out.println("4. Division (/)");
         System.out.print("Select an operation (1/2/3/4): ");
 
+
         int choice = scanner.nextInt();
 
         System.out.print("Enter the first number: ");
@@ -31,4 +32,45 @@ public class calculator {
         scanner.close();
     }
 
+
+        int choice = scanner.nextInt();
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = performOperation(choice, num1, num2);
+
+        if (Double.isNaN(result)) {
+            System.out.println("Error: Invalid operation or division by zero!");
+        } else {
+            System.out.println("Result: " + result);
+        }
+
+        scanner.close();
+    }
+
+    public static double performOperation(int choice, double num1, double num2) {
+        double result = 0.0;
+
+        switch (choice) {git
+            case 1:
+                result = add(num1, num2);
+                break;
+
+            default:
+                result = Double.NaN; // Represents an invalid result
+        }
+
+        return result;
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
 }
