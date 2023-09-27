9e2e39d10999045296351754334a67de8b7576ee
Navneetkumar1998 <navneet.kumar@smarbl.com>
2023-09-27 16:21:44 +0530

    my_add

--- calculator/src/main/java/com/smarbl_classes/calculator.java
+++ calculator/src/main/java/com/smarbl_classes/calculator.java
@@ -1,7 +1,11 @@
 package com.smarbl_classes;
 
+import java.util.Scanner;
+
 public class calculator {
     public static void main(String[] args) {
+
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Simple Calculator");
@@ -22,6 +26,7 @@ public class calculator {
 
         double result = performOperation(choice, num1, num2);
 
+
         if (Double.isNaN(result)) {
             System.out.println("Error: Invalid operation or division by zero!");
         } else {
@@ -29,6 +34,19 @@ public class calculator {
         }
 
         scanner.close();
+    }
+
+    private static double performOperation(int choice, double num1, double num2) {
+        return num1+num2;
+    }
+
+
+    public  void add(int a,int b){
+      System.out.println("addition of two numbers"+(a+b));
+      calculator c=new calculator();
+      c.add(56,784);
+
+
     }
 
 }
