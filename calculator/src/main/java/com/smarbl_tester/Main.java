ce2c17c1c344cd60780442894b8d744ec9c647ff
Navneetkumar1998 <146085673+Navneetkumar1998@users.noreply.github.com>
2023-09-27 16:25:32 +0530

    Navneet kumar brach (#7)
    * text_file_added
    
    * my_add

--- calculator/src/main/java/com/smarbl_tester/Main.java
+++ calculator/src/main/java/com/smarbl_tester/Main.java
@@ -1,4 +1,6 @@
 package com.smarbl_tester;
+
+import com.smarbl_classes.calculator;
 import com.smarbl_tester.Main;
 
 
@@ -9,15 +11,8 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+//        calculator c =new calculator();
+//        c.add(56,78);
 
-        Scanner sc =new Scanner(System.in);
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+8.
-            System.out.println("i = " + i);
         }
     }
-}
